1a3757b0111896f3bede526a3f93281607db3fa5
fluffybunny11 <creepyhobo101@gmail.com>
2016-07-16 13:31:03 -0400

    Update KitsConfiguration.cs

--- Kits-master/KitsConfiguration.cs
+++ Kits-master/KitsConfiguration.cs
@@ -15,9 +15,9 @@ namespace fr34kyn01535.Kits
         {
             GlobalCooldown = 10;
             Kits = new List<Kit>() {
-                new Kit() { Cooldown = 10, Name = "Survival", XP = 0,Items = new List<KitItem>() { new KitItem(245, 1), new KitItem(81, 2), new KitItem(16, 1) }},
-                new Kit() { Cooldown = 10, Name = "Brute Force", XP = 0,Money = 30, Vehicle = 57,Items = new List<KitItem>() { new KitItem(112, 1), new KitItem(113, 3), new KitItem(254, 3) }},
-                new Kit() { Cooldown = 10, Name = "Watcher", XP = 200,Money=-20, Items = new List<KitItem>() { new KitItem(109, 1), new KitItem(111, 3), new KitItem(236, 1) }}
+                new Kit() { Cooldown = 10, Name = "Starter",Items = new List<KitItem>() { new KitItem(245, 1), new KitItem(81, 2), new KitItem(16, 1) }},
+                new Kit() { Cooldown = 10, Name = "Brute Force",Items = new List<KitItem>() { new KitItem(112, 1), new KitItem(113, 3) }},
+                new Kit() { Cooldown = 10, Name = "Watcher", Items = new List<KitItem>() { new KitItem(109, 1), new KitItem(111, 3) }}
             };
         }
     }
